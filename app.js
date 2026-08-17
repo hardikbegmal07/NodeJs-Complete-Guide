223e29366040193e42f515e68de847437fa40fba
begmalhardik <hardikbegmal@gmail.com>
2026-08-17 21:35:17 +0530

    feat(dev): worked on sending message for particular request

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const http = require("http"); // require takes a path to another file or import a core module like http
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // console.log(req.url, req.method, req.headers);
   // process.exit(); but we never tend to exit our server bcz if the server is closed,
   // and a user sends some request it will not read that is why we try not to end a server
+
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>',
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
 
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
