12eef0e66f1f1d6b70e50fd6fb5bb2fb9d13cee9
begmalhardik <hardikbegmal@gmail.com>
2026-08-18 12:01:44 +0530

    feat(dev): worked on parsing request bodies, understanding event driven code and more

--- app.js
+++ app.js
@@ -1,38 +1,20 @@
 const http = require("http"); // require takes a path to another file or import a core module like http
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  // console.log(req.url, req.method, req.headers);
-  // process.exit(); but we never tend to exit our server bcz if the server is closed,
-  // and a user sends some request it will not read that is why we try not to end a server
+const routes = require("./routes");
 
-  const url = req.url;
-  const method = req.method;
+/*
+  An HTTP request body can arrive as a stream of chunks, so Node.js lets us to listen for data events to collect those chunks 
+  and an end event to know when the complete body has arrived.
+*/
 
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>',
-    );
-    res.write("</html>");
-    return res.end();
-  }
+// const server = http.createServer((req, res) => {
+//   // console.log(req.url, req.method, req.headers);
+//   // process.exit(); but we never tend to exit our server bcz if the server is closed,
+//   // and a user sends some request it will not read that is why we try not to end a server
 
-  if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "DUMMY");
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
+// }); // it requires a requestListener
+// // requestListener is a function that will exexute for every incoming request
 
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-  res.write("</html>");
-  res.end();
-}); // it requires a requestListener
-// requestListener is a function that will exexute for every incoming request
+const server = http.createServer(routes.handler);
 
 server.listen(3000); // starts a process and keeps on listening to the incoming request
---
+++ routes.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>',
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    }); // on() allows us to listen to events and here we will be listening to data events.
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      // fs.writeFileSync("message.txt", message); // this is a sync method, so we are blocking the rest of code unit this peice of code execution is done
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
+};
+
+// module.exports = requestHandler;
+
+// module.exports = {
+//   handler: requestHandler,
+//   someText: "some hard coded text",
+// };
+
+// module.exports.handler = requestHandler;
+// module.exports.someText = "some text";
+
+exports.someText = "some text";
+exports.handler = requestHandler;
