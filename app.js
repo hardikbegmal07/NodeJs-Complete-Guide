e70ada4ef23a24d92387205ac4dd0fdae734462b
begmalhardik <hardikbegmal@gmail.com>
2026-08-22 20:56:14 +0530

    feat(dev): worked on basics of express like installation, usecase and advatage over boilerplate code

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const http = require("http"); // require takes a path to another file or import a core module like http
+// const http = require("http"); // require takes a path to another file or import a core module like http
 
-const routes = require("./routes");
+const express = require("express");
+
+// const routes = require("./routes");
 
 // will be working with express.js to learn more on srevers and api
 
@@ -17,6 +19,22 @@ const routes = require("./routes");
 // }); // it requires a requestListener
 // // requestListener is a function that will exexute for every incoming request
 
-const server = http.createServer(routes.handler);
+const app = express();
 
-server.listen(3000); // starts a process and keeps on listening to the incoming request
+app.use((req, res, next) => {
+  console.log("In the middleware!");
+  next(); // allows the request to continue to the next middleware in line
+}); // use() allows us to add a new middleware function,
+// req, res are request and response parameters with some more functions, next is a function
+// it allows the request to continue to the next middleware in line
+
+app.use((req, res, next) => {
+  console.log("In another middleware!");
+  res.send("<h1>Hello from Express!</h1>");
+});
+
+// const server = http.createServer(app); // routes.handler
+// server.listen(3000); // starts a process and keeps on listening to the incoming request
+
+// instead of using above 2 lines, we can now just shorten it to
+app.listen(3000);
