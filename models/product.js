38e6eb11a394dc3a18e3199f6e1496c6769738ff
begmalhardik <hardikbegmal@gmail.com>
2026-08-30 20:59:22 +0530

    feat(dev): controller added to the codebase

---
+++ models/product.js
@@ -0,0 +1,15 @@
+const products = [];
+
+module.exports = class Products {
+  constructor(title) {
+    this.title = title;
+  }
+
+  save() {
+    products.push(this);
+  }
+
+  static fetchAll() {
+    return products;
+  }
+};
